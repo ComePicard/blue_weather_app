8b2288c2a69edafdbc26b388466f6eb4128a343c
ComePicard <come.picard@gmail.com>
2023-01-27 10:23:54 +0100

    ✨ FEAT ManageCity: Ajout du component ManageCity

---
+++ src/components/ManageCity.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+        <v-toolbar color="blue-lighten-2">
+            <v-toolbar-title>
+                <v-icon icon="mdi-magnify"/>
+                Search city
+            </v-toolbar-title>
+        </v-toolbar>
+        <v-text-field
+            outlined>
+        </v-text-field>
+    </v-card>
+</template>
+
+<script>
+
+export default{
+    name: "manageCity",
+}
+</script>
