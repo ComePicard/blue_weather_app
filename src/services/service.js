b40e54dd931fd2d199f73f7d12c018655e589e3e
ComePicard <come.picard@gmail.com>
2023-01-27 17:01:52 +0100

    ✨ FEAT service: ajout d'une méthode pour récupéré les données d'une ville

--- src/services/service.js
+++ src/services/service.js
@@ -1,14 +1,30 @@
 export const getCityByName = async function({
-    name
+    name,
+    number
 }){
-    const response = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + name, {method: "GET"});
+    const response = await fetch("https://geocoding-api.open-meteo.com/v1/search?name=" + name + "&count=" + number, {method: "GET"});
 
     if (response.ok){
         return await response.json();
     }
     else {
         console.log("https://geocoding-api.open-meteo.com/v1/search?name=" + name);
-        throw new Error("Erreur lors du getCity");
+        throw new Error("Erreur lors du getCityByName");
+    }
+};
+
+
+export const getCityCurrentWeather = async function({
+    latitude,
+    longitude
+}){
+    const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=" + latitude + "&longitude=" + longitude + "&current_weather=true", {method: "GET"});
+
+    if (response.ok){
+        return await response.json();
+    }
+    else {
+        throw new Error("Erreur lors du GetCityCurrentWeather");
     }
 };
 
