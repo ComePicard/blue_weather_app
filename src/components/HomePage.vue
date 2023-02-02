3aaf209fd95bf0e13c5a2b1c440a8d45f652f02b
comeP <come.picard@gmail.com>
2023-02-02 20:13:16 +0100

    ✨ FEAT HomePage: -add city -dynamic display of temperature -dinamic display of weather image

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -41,17 +41,18 @@
       <v-container>
         <v-row class="d-flex flex-column align-center">
             <v-col cols="12">
-                <v-chip-group
-                  show-arrows
-                  center-active
-                  mandatory
-                  active-class="blue darken-3 white--text">
-                  <v-chip 
-                    v-for="city in cities"
-                    :key="city">
-                    {{city}}
-                  </v-chip>
-                </v-chip-group>
+              <v-chip-group
+                show-arrows
+                center-active
+                mandatory
+                active-class="blue darken-3 white--text">
+                <v-chip 
+                  v-for="city in cities"
+                  :key="city.name"
+                  @change="selected_city(city)">
+                  {{city.name}}
+                </v-chip>
+              </v-chip-group>
             </v-col>
         </v-row>
         <v-row class="d-flex justify-center mt-16">
@@ -67,7 +68,7 @@
         <v-row class="d-flex justify-center">
           <v-img
             max-width="200px"
-            :src="cloudy_img">
+            :src="img">
           </v-img>
         </v-row>
       </v-container>
@@ -113,7 +114,7 @@
   <script>
   import SearchCity from "@/components/SearchCity.vue";
   import ManageCity from "@/components/ManageCity.vue";
-// import { getCityCurrentWeather } from "@/services/service";
+  import { getCityCurrentWeather } from "@/services/service";
 
   
   export default {
@@ -131,11 +132,10 @@
 
         cityTest : "",
 
-        store: {},
-
-        cities: ["Rennes", "Lannion", "Angers", "Vannes", "Brest"],
-        temperature: 7,
-        weather: "Cloudy",
+        cities: [],
+        temperature: 0,
+        weather: "",
+        img: null,
         cloudy_img: require("../../public/images/cloudy.png"),
         thunder_img: require("../../public/images/thunder.png"),
         sunny_img: require("../../public/images/sunny.png"),
@@ -156,22 +156,102 @@
         longitude
       }){
         try {
-          this.cities.push(name);
-          this.store = longitude + latitude;
-          // this.cities.push(await getCityCurrentWeather({
-          //   latitude: latitude,
-          //   longitude: longitude,
-          // }));
+          this.cities.push(
+            {
+              "name": name,
+              "weather_data":  await getCityCurrentWeather({
+                latitude: latitude,
+                longitude: longitude,
+              })
+            }
+          );
         } catch (e) {
           console.error(e.message);
         }
         finally{
           this.validationDialog();
         }
+      },
+
+      selected_city(item){
+        this.temperature = item["weather_data"]["current_weather"]["temperature"];
+        this.getWeather({weather_code: item["weather_data"]["current_weather"]["weathercode"]})
+      },
+
+      getWeather({weather_code}) {
+        switch (weather_code) {
+          case 0:
+          case 1:
+            this.weather = "Sunny";
+            this.img = this.sunny_img;
+            break;
+          case 2:
+          case 3:
+          case 45:
+          case 48:
+            this.weather = "Cloudy";
+            this.img = this.cloudy_img;
+            break;
+          case 56:
+          case 57:
+          case 61:
+          case 63:
+          case 65:
+          case 66:
+          case 67:
+          case 80:
+          case 81:
+          case 82:
+            this.weather = "Rainy"
+            this.img = this.rainy_img;
+            break;
+          case 71:
+          case 73:
+          case 75:
+          case 77:
+          case 85:
+          case 86:
+            this.weather = "Snowy"
+            this.img = this.snowy_img;
+            break;
+          case 95:
+          case 96:
+          case 99:
+            this.weather = "Thundery"
+            this.img = this.thunder_img;
+            break;
+        }
       }
     },
 
     async created(){
+      await this.addCity({
+        name: "Rennes",
+        latitude: 48.11198,
+        longitude: -1.67429,
+      });
+      await this.addCity({
+        name: "Lannion",
+        latitude: 48.73264,
+        longitude: -3.45657,
+      });
+      await this.addCity({
+        name: "Angers",
+        latitude: 47.47156,
+        longitude: -0.55202,
+      });
+      await this.addCity({
+        name: "Rodez",
+        latitude: 44.35258,
+        longitude: 2.57338,
+      });
+      await this.addCity({
+        name: "Bourg-en-Bresse",
+        latitude: 46.20574,
+        longitude: 5.2258,
+      });
+
+      this.selected_city(this.cities[0])
     }
     
   }
